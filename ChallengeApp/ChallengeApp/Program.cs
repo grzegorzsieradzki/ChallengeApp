0753350c22d0f95b3371b68d478642582ad2b964
grzegorzsieradzki <sieradzki98@protonmail.com>
2023-12-27 22:33:14 +0100

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "kobieta";
+int age = 30;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mężczyzna");
+}
